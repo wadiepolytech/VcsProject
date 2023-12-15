a8ab8edcd67107c5f51ec179f347fee0e16d664d
wadie <sellaouiwadie24@gmail.com>
2023-12-15 20:31:00 +0100

    project init

---
+++ VCSpipeline/src/main/java/com/example/vcspipeline/VcSpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.vcspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcSpipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VcSpipelineApplication.class, args);
+    }
+
+}
---
+++ VCSpipeline/src/test/java/com/example/vcspipeline/VcSpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vcspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcSpipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
